706c038e13cbd3b6624160fcb2fe0af978eb0f91
Aron Huth <aronoh00@zedat.fu-berlin.de>
2024-09-09 13:08:35 +0200

    unittest

--- m_unittest/unittests/test_unittest.py
+++ m_unittest/unittests/test_unittest.py
@@ -1,5 +1,12 @@
 import unittest
+import math
+
 
 class TestStringMethods(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
+
+
+class TestMathMethods(unittest.TestCase):    
+    def test_sqrt(self):
+        self.assertEqual(math.sqrt(10)**2, 10)
