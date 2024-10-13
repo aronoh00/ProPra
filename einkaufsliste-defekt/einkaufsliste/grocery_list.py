26c08c71da05165c6cbfc551368f3390142f639b
Aron Huth <aronoh00@zedat.fu-berlin.de>
2024-10-13 15:30:11 +0200

    grocery_list.py

--- einkaufsliste-defekt/einkaufsliste/grocery_list.py
+++ einkaufsliste-defekt/einkaufsliste/grocery_list.py
@@ -25,11 +25,6 @@ def choose_recipes(recipes):
         except ValueError:
             print("Invalid selection. Please try again.")
 
-def find_location(ingredient_name, all_ingredient_locs):
-    for ing in all_ingredient_locs:
-        if ing["name"] == ingredient_name:
-            return ing["where"]
-
 
 def get_grocery_items(all_ingredient_locs, recipes):
     grocery_items = []
@@ -39,7 +34,7 @@ def get_grocery_items(all_ingredient_locs, recipes):
             {
                 "name": ingr["name"],
                 "quantity": ingr["quantity"],
-                "where": find_location(ingr["name"], all_ingredient_locs)
+                "where": all_ingredient_locs[ingr["name"]],
             }
             for ingr in ingredients
         ]
