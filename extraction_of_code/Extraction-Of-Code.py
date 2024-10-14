0431c986583eb8df5f542353e048d5174ed9d893
Aron Huth <aronoh00@zedat.fu-berlin.de>
2024-10-14 11:06:15 +0200

    Extraction-Of-Code.py

--- extraction_of_code/Extraction-Of-Code.py
+++ extraction_of_code/Extraction-Of-Code.py
@@ -5,9 +5,12 @@ month = random.choice(MONTHS)
 
 
 def what_to_eat(month):
-    if month.lower().endswith("r") or month.lower().endswith("ary"):
+    is_month_ending_with_r = month.lower().endswith("r")
+    is_month_ending_with_ary = month.lower().endswith("ary")
+    is_month_between_may_and_aug = 8 > MONTHS.index(month) > 4
+    if is_month_ending_with_r or is_month_ending_with_ary:
         print(f"{month}: oysters")
-    elif 8 > MONTHS.index(month) > 4:
+    elif is_month_between_may_and_aug:
         print(f"{month}: tomatoes")
     else:
         print(f"{month}: asparagus")
